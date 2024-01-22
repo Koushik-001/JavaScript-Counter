10127d9641a2251ac5c76f84641f4854a29fc2a7
Koushik-001 <89032780+Koushik-001@users.noreply.github.com>
2024-01-22 11:03:18 +0530

    Update Jscript.js

--- JavaScript/Jscript.js
+++ JavaScript/Jscript.js
@@ -1,24 +1,3 @@
-// let x;
-// x="Ferrari 296gtb";
-// console.log(`${x} is my favorite car`);
-// document.getElementById("heAder").textContent=`Hello`;
-// document.getElementById('para').textContent='Java Script tutotrial';
-
-
-// let x;
-// x=window.prompt("Enter the values");
-// document.getElementById("heAder").textContent=x;
-
-
-//  let x;
-//  document.getElementById('submit').onclick = function(){
-//      x=document.getElementById('form').value;
-//      document.getElementById('label').textContent=`Hello ${x}`; 
-//  }
-
-// let x;
-// x=window.prompt("Enter your age");
-// document.getElementById('header').textContent=`${x}`
 
 let count=0;
 document.getElementById("incbtn").onclick = function(){
