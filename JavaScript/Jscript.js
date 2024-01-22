7bbc477b6fff4bd65f2588270890e0bd6619f621
Koushik <ckc9115@gmail.com>
2024-01-22 11:00:00 +0530

    Js Counter

--- JavaScript/Jscript.js
+++ JavaScript/Jscript.js
@@ -10,8 +10,29 @@
 // document.getElementById("heAder").textContent=x;
 
 
-let x;
-document.getElementById('submit').onclick = function(){
-    x=document.getElementById('form').value;
-    document.getElementById('label').textContent=`Hello ${x}`; 
+//  let x;
+//  document.getElementById('submit').onclick = function(){
+//      x=document.getElementById('form').value;
+//      document.getElementById('label').textContent=`Hello ${x}`; 
+//  }
+
+// let x;
+// x=window.prompt("Enter your age");
+// document.getElementById('header').textContent=`${x}`
+
+let count=0;
+document.getElementById("incbtn").onclick = function(){
+    count++;
+    document.getElementById('header').textContent=count;
 }
+document.getElementById("resbtn").onclick = function(){
+    count=0;
+    document.getElementById('header').textContent=count;
+
+}
+document.getElementById("decbtn").onclick = function(){
+    count--;
+    document.getElementById('header').textContent=count;
+
+}
+
